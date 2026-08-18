e7c50a16f9bf567cfeb80f98c5c21b55f45d1ef5
ming12ng-code <ming12ng@gmail.com>
2026-08-18 16:11:31 +0800

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        response = {
+            "message": "Python API is working!"
+        }
+
+        self.wfile.write(
+            json.dumps(response).encode()
+        )
